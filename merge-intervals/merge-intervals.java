e76926d86e9fb2250782189b733a8fd67014ac86
Shubham Vashishtha <16306985+shubhvash@users.noreply.github.com>
2021-12-04 22:36:58 +0530

    Time: 6 ms (63.18%), Space: 41.4 MB (91.78%) - LeetHub

--- merge-intervals/merge-intervals.java
+++ merge-intervals/merge-intervals.java
@@ -1,30 +1,25 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0]-b[0]);
-		
-		List<int[]> result = new ArrayList<>();
-		
-		for(int i=0; i<intervals.length; i++) {
-			if(!result.isEmpty() && 
-			   result.get(result.size()-1)[1] >= intervals[i][0]) {
-				
-				result.get(result.size()-1)[1] = 
-					Math.max(intervals[i][1], result.get(result.size()-1)[1]);
-			} else {
-				result.add(intervals[i]);
-			}
-		}
-		
-		if(result.size() == intervals.length) {
-			return intervals;
-		}
-		
-		int[][] ans = new int[result.size()][2];
-		int i=0;
-		for(int[] interval : result) {
-			ans[i++] = interval;
-		}
-		
-		return ans;
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.offerLast(intervals[0]);
+        
+        for(int[] interval : intervals) {
+            if(dq.peekLast()[1] >= interval[0]) {
+                dq.peekLast()[1] = Math.max(dq.peekLast()[1], interval[1]);
+            } else {
+                dq.offerLast(interval);
+            }
+        }
+        
+        int[][] res = new int[dq.size()][2];
+        int i=0;
+        
+        while(!dq.isEmpty()) {
+            res[i++] = dq.pollFirst();
+        }
+        
+        return res;
     }
 }
